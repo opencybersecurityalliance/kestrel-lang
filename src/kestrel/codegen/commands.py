5a655c9c04fd52ec208ec9b700c4aab993b846f7
Paul Coccoli <pcoccoli@us.ibm.com>
2022-04-21 16:43:35 -0400

    Fix for auto-dereferencing with mixed IPv4/IPv6

--- src/kestrel/codegen/commands.py
+++ src/kestrel/codegen/commands.py
@@ -23,7 +23,8 @@ import logging
 import itertools
 from collections import OrderedDict
 
-from firepit.query import Column, Limit, Offset, Order, Projection, Query
+from firepit.deref import auto_deref
+from firepit.query import Limit, Offset, Order, Projection, Query
 from firepit.stix20 import summarize_pattern
 
 from kestrel.utils import remove_empty_dicts, dedup_ordered_dicts
@@ -613,7 +614,7 @@ def _filter_prefetched_process(
     id_pattern = build_pattern_from_ids(return_type, entity_ids)
     if id_pattern:
         session.store.extract(prefetch_filtered_var_name, return_type, None, id_pattern)
-        _logger.debug(f"filter successful.")
+        _logger.debug("filter successful.")
         return prefetch_filtered_var_name
     else:
         _logger.info("no prefetched process found after filtering.")
@@ -621,35 +622,30 @@ def _filter_prefetched_process(
 
 
 def _set_projection(store, entity_table, query, paths):
-    proj = []
-    cols = store.columns(entity_table)
-    joined = set()
-    for path in paths:
-        if path == "*":
-            return
-        if "_ref" in path and path not in cols:  # This seems like a hack
-            joins, table, column = store.path_joins(entity_table, None, path)
-            if table not in joined:
-                query.extend(joins)
-                joined.add(table)
-            proj.append(Column(column, table, path))
-        elif path in cols:
-            # Prevent any ambiguity
-            proj.append(Column(path, entity_table))
-        else:
-            # Not sure where it came from
-            proj.append(path)
-    query.append(Projection(proj))
+    joins, proj = auto_deref(store, entity_table, paths=paths)
+    query.joins.extend(joins)
+    if query.proj:
+        # Need to merge projections?  More-specific overrides less-specific ("*")
+        new_cols = []
+        for p in query.proj.cols:
+            if not (hasattr(p, "table") and p.table == entity_table and p.name == "*"):
+                new_cols.append(p)
+        for p in proj.cols:
+            if not (hasattr(p, "table") and p.table == entity_table and p.name == "*"):
+                new_cols.append(p)
+        query.proj = Projection(new_cols)
+    else:
+        query.proj = proj
 
 
 def _build_query(store, entity_table, qry, stmt):
     where = stmt.get("where")
     if where:
+        where.set_table(entity_table)
         qry.append(where)
     attrs = stmt.get("attrs", "*")
-    if attrs != "*":
-        cols = attrs.split(",")
-        _set_projection(store, entity_table, qry, cols)
+    cols = attrs.split(",")
+    _set_projection(store, entity_table, qry, cols)
     sort_by = stmt.get("path")
     if sort_by:
         direction = "ASC" if stmt["ascending"] else "DESC"
--- tests/test_command_disp.py
+++ tests/test_command_disp.py
@@ -1,7 +1,15 @@
+import os
+import pandas as pd
 import pytest
 
 from kestrel.exceptions import VariableNotExist
 from kestrel.session import Session
+
+
+@pytest.fixture
+def proc_bundle_file():
+    cwd = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(cwd, "doctored-1k.json")
 
 
 def test_disp():
@@ -50,3 +58,23 @@ newvar = NEW [ {"type": "network-traffic", "src_ref.value": "1.2.3.4", "dst_ref.
         out = s.execute("DISP grpvar")
         data = out[0].to_dict()["data"]
         assert len(data) == 2
+
+
+def test_disp_mixed_v4_v6(proc_bundle_file):
+    with Session() as s:
+        stmt = f"""
+conns = GET network-traffic
+        FROM file://{proc_bundle_file}
+        WHERE [network-traffic:dst_port > 0]
+"""
+        s.execute(stmt)
+
+        out = s.execute("DISP conns ATTR src_ref.value, src_port")
+        data = out[0].to_dict()["data"]
+        df = pd.DataFrame.from_records(data)
+        assert df.columns.tolist() == ["src_ref.value", "src_port"]
+
+        out = s.execute("DISP TIMESTAMPED(conns) ATTR src_ref.value, src_port")
+        data = out[0].to_dict()["data"]
+        df = pd.DataFrame.from_records(data)
+        assert df.columns.tolist() == ["first_observed", "src_ref.value", "src_port"]
