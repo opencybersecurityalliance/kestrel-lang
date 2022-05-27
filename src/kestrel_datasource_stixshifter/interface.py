afc68f3ab109e75e6404b13d0248a9538c039828
Xiaokui Shu <subbyte@gmail.com>
2022-05-27 15:19:13 -0400

    fully fixed #230 and add an exception handling

--- src/kestrel_datasource_stixshifter/interface.py
+++ src/kestrel_datasource_stixshifter/interface.py
@@ -170,7 +170,7 @@ class StixShifterInterface(AbstractDataSourceInterface):
 
             if "error" in dsl:
                 raise DataSourceError(
-                    f"STIX-shifter translation failed with message: {dsl['error']}"
+                    f"STIX-shifter translation from STIX to native query failed with message: {dsl['error']}"
                 )
 
             _logger.debug(f"STIX pattern to query: {pattern}")
@@ -243,8 +243,13 @@ class StixShifterInterface(AbstractDataSourceInterface):
                 "results",
                 query_metadata,
                 json.dumps(connector_results),
-                {},
+                translation_options,
             )
+
+            if "error" in stixbundle:
+                raise DataSourceError(
+                    f"STIX-shifter translation results to STIX failed with message: {stixbundle['error']}"
+                )
 
             _logger.debug(f"dumping STIX bundles into file: {ingestfile}")
             with ingestfile.open("w") as ingest:
