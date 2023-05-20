4078fe7cad733c8d1b1c314a3bcd7ead2fc6d281
leila <leila.rashidi.lr@gmail.com>
2023-05-20 15:53:40 -0300

    adaptedToNewSQLStorage_firepit

--- src/kestrel/codegen/commands.py
+++ src/kestrel/codegen/commands.py
@@ -114,9 +114,11 @@ def assign(stmt, session):
     transform = stmt.get("transform") or stmt.get("transform2")
     if transform:
         if transform.lower() == "timestamped":
-            qry = session.store.timestamped(entity_table, run=False)
+            qry = session.store.extract_observeddata_attribute(
+                entity_table, name_of_attribute="first_observed", run=False
+            )
         elif transform.lower() == "observed":
-            qry = session.store.get_all_nested_objects_including_an_attribute_of_SCO(
+            qry = session.store.extract_observeddata_attribute(
                 entity_table, name_of_attribute="id", run=False
             )
         else:
@@ -221,9 +223,11 @@ def disp(stmt, session):
     transform = stmt.get("transform") or stmt.get("transform2")
     if transform and entity_table:
         if transform.lower() == "timestamped":
-            qry = session.store.timestamped(entity_table, run=False)
+            qry = session.store.extract_observeddata_attribute(
+                entity_table, name_of_attribute="first_observed", run=False
+            )
         elif transform.lower() == "observed":
-            qry = session.store.get_all_nested_objects_including_an_attribute_of_SCO(
+            qry = session.store.extract_observeddata_attribute(
                 entity_table, name_of_attribute="id", run=False
             )
         else:
