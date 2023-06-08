06c6a5cd52f66b039039cca06efb15ca293efeb3
Xiaokui Shu <subbyte@gmail.com>
2023-06-08 10:16:25 -0400

    fix conflict between multiprocessing and runpy

--- src/kestrel/__init__.py
+++ src/kestrel/__init__.py
@@ -1,2 +1,3 @@
 from kestrel.session import Session
 from kestrel.syntax.utils import get_keywords
+from kestrel.cli import cli
--- src/kestrel/__main__.py
+++ src/kestrel/__main__.py
@@ -4,37 +4,7 @@
 # Usage: `python3 -m kestrel [-v] [--debug] hunting.hf`
 ################################################################
 
-import argparse
-import logging
-
-from kestrel.session import Session
-from kestrel.utils import add_logging_handler, clear_logging_handlers
-
-_logger = logging.getLogger(__name__)
-
-
-def logging_setup(if_verbose_mode, if_debug_mode):
-    clear_logging_handlers()
-    if if_verbose_mode:
-        add_logging_handler(logging.StreamHandler(), if_debug_mode)
-
+from kestrel import cli
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Kestrel Interpreter")
-    parser.add_argument("huntflow", help="huntflow in .hf file")
-    parser.add_argument(
-        "-v", "--verbose", help="print verbose log", action="store_true"
-    )
-    parser.add_argument(
-        "--debug", help="debug level log (default is info level)", action="store_true"
-    )
-    args = parser.parse_args()
-
-    logging_setup(args.verbose, args.debug)
-
-    with Session(debug_mode=args.debug) as session:
-        with open(args.huntflow, "r") as fp:
-            huntflow = fp.read()
-        outputs = session.execute(huntflow)
-        results = "\n\n".join([o.to_string() for o in outputs])
-        print(results)
+    cli()
