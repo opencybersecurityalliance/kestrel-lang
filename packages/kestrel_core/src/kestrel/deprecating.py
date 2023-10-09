37eb7c0a8cc43b8046f923dd0289ff6e19297db5
Xiaokui Shu <subbyte@gmail.com>
2023-10-09 16:42:45 -0400

    fix Python 3.8 lark loading issue

--- packages/kestrel_core/src/kestrel/deprecating.py
+++ packages/kestrel_core/src/kestrel/deprecating.py
@@ -5,9 +5,9 @@ from importlib import resources
 def load_data_file(package_name, file_name):
     try:
         # resources.files() is introduced in Python 3.9
-        default_config = resources.files(package_name).joinpath(file_name).read_text()
+        content = resources.files(package_name).joinpath(file_name).read_text()
     except AttributeError:
         # Python 3.8; deprecation warning forward
-        default_config = get_data(package_name, file_name)
+        content = get_data(package_name, file_name).decode("utf-8")
 
-    return default_config
+    return content
