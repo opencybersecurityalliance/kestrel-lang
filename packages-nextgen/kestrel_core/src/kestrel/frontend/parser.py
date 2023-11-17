ec71da59cbe8952d171fa3df2fa7aa21aad5d4ce
Paul Coccoli <pcoccoli@us.ibm.com>
2023-11-17 14:26:35 -0500

    add load_data_file util function to nextgen kestrel_core

--- packages-nextgen/kestrel_core/src/kestrel/frontend/parser.py
+++ packages-nextgen/kestrel_core/src/kestrel/frontend/parser.py
@@ -1,17 +1,17 @@
 # parse Kestrel syntax, apply frontend mapping, transform to IR
 
-import os
-
 from lark import Lark
 
 from kestrel.frontend.compile import _KestrelT
+from kestrel.utils import load_data_file
 
 
 # Create a single, reusable transformer
-with open(
-    os.path.join(os.path.dirname(os.path.abspath(__file__)), "kestrel.lark"), "r"
-) as fp:
-    _parser = Lark(fp, parser="lalr", transformer=_KestrelT())
+_parser = Lark(
+    load_data_file("kestrel.frontend", "kestrel.lark"),
+    parser="lalr",
+    transformer=_KestrelT(),
+)
 
 
 def parse_kestrel(stmts):
