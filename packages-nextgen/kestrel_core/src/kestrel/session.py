19205b6d978d76557604827e0f63c0e2eda3fdba
Xiaokui Shu <subbyte@gmail.com>
2023-12-21 12:34:51 -0500

    comments on session.im

--- packages-nextgen/kestrel_core/src/kestrel/session.py
+++ packages-nextgen/kestrel_core/src/kestrel/session.py
@@ -1,14 +1,16 @@
-import uuid
 import logging
 import atexit
 
+from typing import Mapping
 from typeguard import typechecked
 from contextlib import AbstractContextManager
+from uuid import UUID, uuid4
 
 from kestrel.ir.graph import IRGraph
 from kestrel.frontend.parser import parse_kestrel
-from kestrel.cache.sqlite import SqliteCache
+from kestrel.cache import AbstractCache, SqliteCache
 from kestrel.ir.instructions import Variable, CACHE_INTERFACE_IDENTIFIER
+from kestrel.interface.datasource import AbstractDataSourceInterface
 
 
 _logger = logging.getLogger(__name__)
@@ -19,12 +21,17 @@ class Session(AbstractContextManager):
     """Kestrel huntflow execution session"""
 
     def __init__(self):
-        self.session_id = uuid.uuid4()
-        self.irgraph = IRGraph()
-        self.cache = SqliteCache()
+        self.session_id: UUID = uuid4()
+        self.irgraph: IRGraph = IRGraph()
+        self.cache: AbstractCache = SqliteCache()
 
         # interface mapper
-        self.im = {CACHE_INTERFACE_IDENTIFIER: self.cache}
+        # Select a datasource interface given its name
+        # Note that cache is a special datasource interface and should always be added
+        # TODO: other datasource interfaces to initialize/add if exist
+        self.im: Mapping[str, AbstractDataSourceInterface] = {
+            CACHE_INTERFACE_IDENTIFIER: self.cache
+        }
 
     def execute(self, huntflow_block: str):
         """Execute a Kestrel huntflow block.
@@ -35,10 +42,10 @@ class Session(AbstractContextManager):
         referred in the new huntflow block.
 
         Parameters:
-            huntflow_block: the new huntflow block to be executed.
+            huntflow_block: the new huntflow block to be executed
 
         Yields:
-            Evaluated result per Return instruction in the huntflow block.
+            Evaluated result per Return instruction in the huntflow block
         """
         irgraph_new = parse_kestrel(huntflow_block)
         self.irgraph.update(irgraph_new)
@@ -56,11 +63,11 @@ class Session(AbstractContextManager):
         """Kestrel code auto-completion.
 
         Parameters:
-            huntflow_block: Kestrel code.
-            cursor_pos: the position to start completion (index in ``huntflow_block``).
+            huntflow_block: Kestrel code
+            cursor_pos: the position to start completion (index in ``huntflow_block``)
 
         Returns:
-            A list of suggested strings to complete the code.
+            A list of suggested strings to complete the code
         """
         raise NotImplementedError()
 
