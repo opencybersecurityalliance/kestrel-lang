91302db5e6107966fabe55972bf2065a00f17499
Xiaokui Shu <subbyte@gmail.com>
2024-01-24 21:22:42 -0500

    minor fix in test

--- packages-nextgen/kestrel_core/tests/test_ir_filter.py
+++ packages-nextgen/kestrel_core/tests/test_ir_filter.py
@@ -139,7 +139,6 @@ def test_fill_references_in_exp():
     rhs = RefComparison("baz", "=", ReferenceValue("var", "attr"))
     exp = BoolExp(lhs, ExpOp.AND, rhs)
     rs = get_references_from_exp(exp)
-    r2v = {r: 5 for r in rs}
     assert len(r2v) == 1
-    resolve_reference_with_function(exp, lambda x: r2v[x])
+    resolve_reference_with_function(exp, lambda x: 5)
     assert exp.rhs.value == 5
